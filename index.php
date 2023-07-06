063e76a9c61e8a00a9de86c7fbf526f58cb97afb
Charles-Muia <Charles.K.Muia@gmail.com>
2023-07-06 14:40:25 +0300

    Real_Estate

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<!--===CSS------------------------------------------------------------------------------------------------------------->
+    <link rel="stylesheet" href="style.css">
+
+<!--===FAVICON--------------------------------------------------------------------------------------------------------->
+    <link rel="icon" href="Images/Logo_3.png">
+
+<!--===CDN ICONS------------------------------------------------------------------------------------------------------->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+<!--===GOOGLE FONTS---------------------------------------------------------------------------------------------------->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet">
+
+    <title>Home | A place To Live</title>
+</head>
+<body>
+
+
+
+<!--===JS---------------------------------------------------------------------------------------------------->
+
+<script></script>
+</body>
+</html>
